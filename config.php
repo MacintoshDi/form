06c4a728495a1a6c4ab5d5ce88c2a380363515f6
MacintoshDi <74073116+MacintoshDi@users.noreply.github.com>
2023-03-31 02:46:41 +0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DB_HOST', 'localhost'); // адрес сервера базы данных
-define('DB_NAME', 'ch7af6ded2_comment'); // имя базы данных
-define('DB_USER', 'ch7af6ded2_comment'); // имя пользователя базы данных
-define('DB_PASS', ''); // пароль пользователя базы данных
-
-// Подключаемся к базе данных MySQL
-$pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
-?>
