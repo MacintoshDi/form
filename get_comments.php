7b5f3ee34582d38d37fddf5576bef2f22b41e89d
MacintoshDi <74073116+MacintoshDi@users.noreply.github.com>
2023-03-31 01:45:28 +0300

    Update get_comments.php

--- get_comments.php
+++ get_comments.php
@@ -1,44 +1,19 @@
 <?php
+session_start();
 
-// Подключаем файл с настройками базы данных
-require_once 'config.php';
+// Получаем список комментариев
+$comments = $_SESSION['comments'] ?? [];
 
-// Проверяем, что была отправлена форма и что все поля заполнены
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['name']) && !empty($_POST['comment']) && !empty($_POST['rating']) && !empty($_POST['captcha'])) {
-
-  // Проверяем правильность введенного значения в поле captcha
-  if ($_POST['captcha'] == $_SESSION['captcha']) {
-
-    // Создаем подключение к базе данных
-    $conn = new mysqli($host, $username, $password, $database);
-
-    // Проверяем, удалось ли подключиться к базе данных
-    if ($conn->connect_error) {
-      die("Ошибка подключения к базе данных: " . $conn->connect_error);
-    }
-
-    // Защищаем данные, полученные из формы, от SQL-инъекций
-    $name = $conn->real_escape_string($_POST['name']);
-    $comment = $conn->real_escape_string($_POST['comment']);
-    $rating = $conn->real_escape_string($_POST['rating']);
-    $photo = $conn->real_escape_string($_FILES['photo']['name']);
-
-    // Создаем SQL-запрос для добавления комментария в базу данных
-    $sql = "INSERT INTO comments (name, comment, rating, photo) VALUES ('$name', '$comment', '$rating', '$photo')";
-
-    // Выполняем запрос
-    if ($conn->query($sql) === TRUE) {
-      // Выводим сообщение об успешном добавлении комментария
-      echo "Комментарий успешно добавлен!";
-    } else {
-      // Выводим сообщение об ошибке при добавлении комментария
-      echo "Ошибка: " . $sql . "<br>" . $conn->error;
-    }
-
-    // Закрываем соединение с базой данных
-    $conn->close();
-
-  } else {
-    echo "Введенный код с картинки неверный";
+// Выводим список комментариев
+foreach ($comments as $comment) {
+  echo '<li>';
+  echo '<div class="comment">';
+  echo '<div class="comment__author">' . htmlspecialchars($comment['name']) . '</div>';
+  echo '<div class="comment__text">' . htmlspecialchars($comment['comment']) . '</div>';
+  echo '<div class="comment__rating">' . htmlspecialchars($comment['rating']) . '</div>';
+  if (!empty($comment['photo'])) {
+    echo '<div class="comment__photo"><img src="' . htmlspecialchars($comment['photo']) . '"></div>';
   }
+  echo '</div>';
+  echo '</li>';
 }
